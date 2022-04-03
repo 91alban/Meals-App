974183b1dfbe8cb1665979a05050f6814554b862
91alban <aloku93@gmail.com>
2022-04-03 17:03:19 +0200

    added meals models

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/category_meals_screen.dart';
+
+import '../screens/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
-  // const CategoryItem({Key? key}) : super(key: key);
   final String id;
   final String title;
   final Color color;
 
-  const CategoryItem(
-      {Key? key, required this.id, required this.title, required this.color})
-      : super(key: key);
+  CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx)
-        .pushNamed('/category-meals', arguments: {'id': id, 'title': title});
+    Navigator.of(ctx).pushNamed(
+      CategoryMealsScreen.routeName,
+      arguments: {
+        'id': id,
+        'title': title,
+      },
+    );
   }
 
   @override
@@ -30,7 +33,10 @@ class CategoryItem extends StatelessWidget {
         ),
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [color.withOpacity(0.7), color],
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
           ),
